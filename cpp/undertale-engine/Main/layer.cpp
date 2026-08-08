f5e850e09017bf7de45a5fb644d36d87480937c1
wheedo07 <wheedo07@ourgram.co.kr>
2026-08-08 11:28:30 +0900

    수정

--- cpp/undertale-engine/Main/layer.cpp
+++ cpp/undertale-engine/Main/layer.cpp
@@ -1,86 +1,24 @@
 #include "layer.h"
 #include "env.h"
-#include<godot_cpp/classes/project_settings.hpp>
-#include<godot_cpp/classes/viewport_texture.hpp>
 using namespace godot;
 
 void UTGELayer::_bind_methods() {
-    BIND_ENUM_CONSTANT(DIRECT);
-    BIND_ENUM_CONSTANT(SUBVIEWPORT);
-
     ClassDB::bind_method(D_METHOD("set_layer_id", "value"), &UTGELayer::set_layer_id);
     ClassDB::bind_method(D_METHOD("get_layer_id"), &UTGELayer::get_layer_id);
     ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "layer_id"), "set_layer_id", "get_layer_id");
-
-    ClassDB::bind_method(D_METHOD("set_render_mode", "value"), &UTGELayer::set_render_mode);
-    ClassDB::bind_method(D_METHOD("get_render_mode"), &UTGELayer::get_render_mode);
-    ADD_PROPERTY(PropertyInfo(Variant::INT, "render_mode", PROPERTY_HINT_ENUM, "Direct,SubViewport", PROPERTY_USAGE_DEFAULT), "set_render_mode", "get_render_mode");
 
     ClassDB::bind_method(D_METHOD("change_scene", "scene"), &UTGELayer::change_scene);
     ClassDB::bind_method(D_METHOD("get_current_scene"), &UTGELayer::get_current_scene);
     ClassDB::bind_method(D_METHOD("clear"), &UTGELayer::clear);
     ClassDB::bind_method(D_METHOD("pause"), &UTGELayer::pause);
     ClassDB::bind_method(D_METHOD("unpause"), &UTGELayer::unpause);
-    ClassDB::bind_method(D_METHOD("is_paused"), &UTGELayer::is_paused);
-}
-
-void UTGELayer::_get_property_list(List<PropertyInfo> *p_list) const {
-    if(render_mode == SUBVIEWPORT) {
-        p_list->push_back(PropertyInfo(Variant::NIL, "SubViewport Settings", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_CATEGORY));
-        p_list->push_back(PropertyInfo(Variant::INT, "default_texture_filter", PROPERTY_HINT_ENUM, "Nearest,Linear,Linear with Mipmaps,Nearest with Mipmaps"));
-        p_list->push_back(PropertyInfo(Variant::INT, "default_texture_repeat", PROPERTY_HINT_ENUM, "Disabled,Enabled,Mirror"));
-    }
-}
-
-bool UTGELayer::_set(const StringName& p_name, const Variant& p_value) {
-    String name = p_name;
-    if(name == "default_texture_filter") {
-        default_texture_filter = SubViewport::DefaultCanvasItemTextureFilter(int(p_value));
-        _apply_viewport_settings();
-        return true;
-    }else if(name == "default_texture_repeat") {
-        default_texture_repeat = SubViewport::DefaultCanvasItemTextureRepeat(int(p_value));
-        _apply_viewport_settings();
-        return true;
-    }
-    return false;
-}
-
-bool UTGELayer::_get(const StringName& p_name, Variant& r_ret) {
-    String name = p_name;
-    if(name == "default_texture_filter") {
-        r_ret = default_texture_filter;
-        return true;
-    }else if(name == "default_texture_repeat") {
-        r_ret = default_texture_repeat;
-        return true;
-    }
-    return false;
-}
-
-void UTGELayer::_ready() {
-    if(isEditor) return;
-    if(render_mode == DIRECT) {
-        scene_root = this;
-    }else if(render_mode == SUBVIEWPORT) {
-        SubViewport *viewport = memnew(SubViewport);
-        add_child(viewport);
-        scene_root = viewport;
-        _apply_viewport_settings();
-
-        display = memnew(TextureRect);
-        display->set_texture(viewport->get_texture());
-        display->set_anchors_preset(Control::PRESET_FULL_RECT);
-        add_child(display);
-    }
 }
 
 Node *UTGELayer::change_scene(Ref<PackedScene> scene) {
-    if(!scene_root) return nullptr;
     ERR_FAIL_COND_V(scene.is_null(), nullptr);
     clear();
     current_scene = scene->instantiate();
-    scene_root->add_child(current_scene);
+    add_child(current_scene);
     return current_scene;
 }
 
@@ -95,38 +33,9 @@ void UTGELayer::clear() {
 }
 
 void UTGELayer::pause() {
-    if(!scene_root || !scene_root->is_class("SubViewport")) return;
-    SubViewport *viewport = Object::cast_to<SubViewport>(scene_root);
-    ERR_FAIL_NULL(viewport);
-    viewport->set_update_mode(SubViewport::UPDATE_DISABLED);
-    viewport->set_process_mode(Node::PROCESS_MODE_DISABLED);
-    isPaused = true;
 }
 
 void UTGELayer::unpause() {
-    if(!scene_root || !scene_root->is_class("SubViewport")) return;
-    SubViewport *viewport = Object::cast_to<SubViewport>(scene_root);
-    ERR_FAIL_NULL(viewport);
-    viewport->set_update_mode(SubViewport::UPDATE_ALWAYS);
-    viewport->set_process_mode(Node::PROCESS_MODE_INHERIT);
-    isPaused = false;
-}
-
-bool UTGELayer::is_paused() {
-    return isPaused;
-}
-
-void UTGELayer::_apply_viewport_settings() {
-    if(!scene_root || !scene_root->is_class("SubViewport")) return;
-    ProjectSettings *setting = ProjectSettings::get_singleton();
-    int width = int(setting->get_setting("display/window/size/viewport_width"));
-    int height = int(setting->get_setting("display/window/size/viewport_height"));
-
-    SubViewport *viewport = Object::cast_to<SubViewport>(scene_root);
-    viewport->set_default_canvas_item_texture_filter(default_texture_filter);
-    viewport->set_default_canvas_item_texture_repeat(default_texture_repeat);
-    viewport->set_transparent_background(true);
-    viewport->set_size(Vector2(width, height));
 }
 
 void UTGELayer::set_layer_id(StringName value) {
@@ -135,13 +44,4 @@ void UTGELayer::set_layer_id(StringName value) {
 
 StringName UTGELayer::get_layer_id() {
     return layer_id;
-}
-
-void UTGELayer::set_render_mode(RenderMode value) {
-    render_mode = value;
-    notify_property_list_changed();
-}
-
-UTGELayer::RenderMode UTGELayer::get_render_mode() {
-    return render_mode;
 }
--- cpp/undertale-engine/Main/layer.h
+++ cpp/undertale-engine/Main/layer.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include<godot_cpp/classes/node.hpp>
-#include<godot_cpp/classes/sub_viewport.hpp>
-#include<godot_cpp/classes/texture_rect.hpp>
 #include<godot_cpp/classes/packed_scene.hpp>
 namespace godot {
     class UTGELayer : public Node {
@@ -11,35 +9,12 @@ namespace godot {
         protected:
             static void _bind_methods();
 
-        public:
-            enum RenderMode {
-                DIRECT,
-                SUBVIEWPORT
-            };
-
         private:
             StringName layer_id = "main";
-            RenderMode render_mode = DIRECT;
 
-            SubViewport::DefaultCanvasItemTextureFilter default_texture_filter =
-            SubViewport::DefaultCanvasItemTextureFilter::DEFAULT_CANVAS_ITEM_TEXTURE_FILTER_LINEAR;
-
-            SubViewport::DefaultCanvasItemTextureRepeat default_texture_repeat =
-            SubViewport::DefaultCanvasItemTextureRepeat::DEFAULT_CANVAS_ITEM_TEXTURE_REPEAT_DISABLED;
-
-            bool isPaused = false;
-            Node *scene_root = nullptr, *current_scene = nullptr;
-            TextureRect *display = nullptr;
-
-            void _apply_viewport_settings();
+            Node *current_scene = nullptr;
 
         public:
-            void _get_property_list(List<PropertyInfo> *p_list) const;
-            bool _set(const StringName& p_name, const Variant& p_value);
-            bool _get(const StringName& p_name, Variant& r_ret);
-
-            void _ready() override;
-
             /* API 함수 */
 
             Node *change_scene(Ref<PackedScene> scene);
@@ -48,14 +23,9 @@ namespace godot {
 
             void pause();
             void unpause();
-            bool is_paused();
 
             /* setters / getters */
             void set_layer_id(StringName value);
             StringName get_layer_id();
-
-            void set_render_mode(RenderMode value);
-            RenderMode get_render_mode();
     };
 }
-VARIANT_ENUM_CAST(godot::UTGELayer::RenderMode);
